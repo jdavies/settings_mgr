6b75342328d283a93e193c5d25c796cb28d27920
Jeff Davies <297151+jdavies@users.noreply.github.com>
2023-03-17 14:47:12 -0700

    added test for color values

--- test/test.py
+++ test/test.py
@@ -1,21 +1,30 @@
 import json
 
-print('Saving a JSON file')
 
-data = {
-    "name" : {
-        "first" : "First",
-        "second" : "Second",
-    },
-    "age" : 32
-}
+def testColor():
+    color = (1,2,3)
+    print(color)
+    print(color[1])
 
-# Create a new root level attribute
-data['test'] = 'This is a test'
-# Create a new object section
-data['render_props'] = {}
-# Add an attibute to the new section.
-data['render_props']['engine'] = 'cycles'
+def testJSON():
+    print('Saving a JSON file')
 
-with open('test.json', 'w', encoding='utf-8') as f:
-    json.dump(data, f, sort_keys=True, ensure_ascii=True, indent=4)
+    data = {
+        "name" : {
+            "first" : "First",
+            "second" : "Second",
+        },
+        "age" : 32
+    }
+
+    # Create a new root level attribute
+    data['test'] = 'This is a test'
+    # Create a new object section
+    data['render_props'] = {}
+    # Add an attibute to the new section.
+    data['render_props']['engine'] = 'cycles'
+
+    with open('test.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, sort_keys=True, ensure_ascii=True, indent=4)
+
+testColor()
