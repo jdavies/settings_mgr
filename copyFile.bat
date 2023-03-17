c17e15fe9cf8c232bf3010798ee4effae2f2f352
Jeff Davies <297151+jdavies@users.noreply.github.com>
2023-03-17 14:46:53 -0700

    fixed copy command to only copy .py files

--- copyFile.bat
+++ copyFile.bat
@@ -1,3 +1,3 @@
 @echo on
-copy differ\*.* "C:\\Users\\jeff\\AppData\\Roaming\\Blender Foundation\\Blender\\3.4\\scripts\\addons\\settings_mgr"
+copy settings_mgr\*.py "C:\\Users\\jeff\\AppData\\Roaming\\Blender Foundation\\Blender\\3.4\\scripts\\addons\\settings_mgr"
 pause
