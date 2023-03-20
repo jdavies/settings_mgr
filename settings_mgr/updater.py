da05fd2baabf768b7334961eb8142f7c902d4ccb
jdavies <jeff.davies@yahoo.com>
2023-03-19 22:17:21 -0700

    minor output formatting changes

--- settings_mgr/updater.py
+++ settings_mgr/updater.py
@@ -4,7 +4,7 @@ import urllib.request
 
 # currentVersion is a string formatted like "1.2.3". No leading zeros
 def isUpdateAvailable(currentVersionTuple):
-    # print("checking for updates...")
+    print("checking for updates...")
     result = False   # Default to no update
     with urllib.request.urlopen('https://raw.githubusercontent.com/jdavies/settings_mgr/master/settings_mgr/version.txt') as response:
         html = response.read()
@@ -14,7 +14,7 @@ def isUpdateAvailable(currentVersionTuple):
         currentVersion = int(currentVersionTuple[0]) * 100 + int(currentVersionTuple[1]) * 10 + int(currentVersionTuple[2])
         print("comparing " + str(latestVersion) + " = " + str(latestVersion) + " to " + str(currentVersion) + " - " + str(currentVersionTuple))
         result = latestVersion > currentVersion
-        # print("Result: " + str(result))
+        print("   Result: " + str(result))
     return result
 
 if __name__ == "__main__":
@@ -22,12 +22,12 @@ if __name__ == "__main__":
     # This will nott work when testing aagainst the live 
     # version number on Github
     print(isUpdateAvailable((0, 0, 2)))
-    assert isUpdateAvailable((0, 0, 2)) == True
-    print(isUpdateAvailable((0, 0, 3)))
-    print(isUpdateAvailable((1, 0, 0)))
-    print(isUpdateAvailable((0, 1, 1)))
-    print(isUpdateAvailable((2, 0, 0)))
-    assert isUpdateAvailable((2, 0, 0)) == False
-    print(isUpdateAvailable((1, 1, 0)))
-    print(isUpdateAvailable((1, 0, 3)))
-    print(isUpdateAvailable((1, 0, 2)))
+    assert isUpdateAvailable((0, 0, 2)) == False
+    # print(isUpdateAvailable((0, 0, 3)))
+    # print(isUpdateAvailable((1, 0, 0)))
+    # print(isUpdateAvailable((0, 1, 1)))
+    # print(isUpdateAvailable((2, 0, 0)))
+    # assert isUpdateAvailable((2, 0, 0)) == False
+    # print(isUpdateAvailable((1, 1, 0)))
+    # print(isUpdateAvailable((1, 0, 3)))
+    # print(isUpdateAvailable((1, 0, 2)))
