dab200dda6e63d05c081fd835622974b97f473d6
jdavies <jeff.davies@yahoo.com>
2023-03-27 13:58:35 -0700

    Got rid of the sub-panel approach

--- settings_mgr/settings_mgr.py
+++ settings_mgr/settings_mgr.py
@@ -58,6 +58,9 @@ class NPanel(BasePanel, bpy.types.Panel):
         row.prop(my_props, "load_pref_texture")
 
         row = box.row()
+        row.prop(my_props, "load_dummy")
+        
+        row = box.row()
         row.prop(my_props, "load_filename")
         loadFileOp = row.operator(ImportSomeData.bl_idname, text="", icon='FILEBROWSER')
         loadFileOp.isSaveFile = False   # False = We are loading data
@@ -69,29 +72,29 @@ class NPanel(BasePanel, bpy.types.Panel):
     #     save_file = self.save_file_name
     #     print("save_file = " + save_file)
 
-class RenderPropsSubPanel(BasePanel, bpy.types.Panel):
-    """Creates a child N-Panel in the Object properties window"""
-    bl_options = {"DEFAULT_CLOSED"}
-    # Panel Text
-    bl_parent_id = "OBJECT_PT_settings" # Use the bl_idname of the parent!
-    bl_label = "Render Properties"
-    bl_idname = "OBJECT_PT_renderprops"
+# class RenderPropsSubPanel(BasePanel, bpy.types.Panel):
+#     """Creates a child N-Panel in the Object properties window"""
+#     bl_options = {"DEFAULT_CLOSED"}
+#     # Panel Text
+#     bl_parent_id = "OBJECT_PT_settings" # Use the bl_idname of the parent!
+#     bl_label = "Render Properties"
+#     bl_idname = "OBJECT_PT_renderprops"
 
-    def draw(self, context):
-        # Do the layout
-        layout = self.layout
-        scene = context.scene
-        # obj = context.object
-        my_props = scene.my_props
+#     def draw(self, context):
+#         # Do the layout
+#         layout = self.layout
+#         scene = context.scene
+#         # obj = context.object
+#         my_props = scene.my_props
         
-        # Render Props section
-        box = layout.box()
+#         # Render Props section
+#         box = layout.box()
 
-        row = box.row()
-        row.prop(my_props, "load_pref_render")
-        row.prop(my_props, "load_pref_output")
-        row.prop(my_props, "load_pref_view_layer")
-        row.prop(my_props, "load_pref_scene")
-        row.prop(my_props, "load_pref_world")
-        row.prop(my_props, "load_pref_collection")
-        row.prop(my_props, "load_pref_texture")
+#         row = box.row()
+#         row.prop(my_props, "load_pref_render")
+#         row.prop(my_props, "load_pref_output")
+#         row.prop(my_props, "load_pref_view_layer")
+#         row.prop(my_props, "load_pref_scene")
+#         row.prop(my_props, "load_pref_world")
+#         row.prop(my_props, "load_pref_collection")
+#         row.prop(my_props, "load_pref_texture")
