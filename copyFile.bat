e4d34123eecc977c0ec781cb716485c6d2e3e89f
jdavies <jeff.davies@yahoo.com>
2023-06-19 10:49:48 -0700

    updated copy destination

--- copyFile.bat
+++ copyFile.bat
@@ -1,3 +1,4 @@
 @echo on
 copy settings_mgr\*.py "C:\Users\jeffd\AppData\Roaming\Blender Foundation\Blender\3.4\scripts\addons\settings_mgr"
+copy settings_mgr\*.py "C:\Users\jeffd\AppData\Roaming\Blender Foundation\Blender\3.5\scripts\addons\settings_mgr"
 pause
